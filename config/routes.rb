b20ded982c979837f3ef8d5540d8f7eea186e0b2
Sauva3k <sauva3k@hotmail.com>
2017-03-30 22:49:19 +1100

    Finish home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'home_page#home'
+  
   get 'home_page/home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#tlbhome'
+  
 end
